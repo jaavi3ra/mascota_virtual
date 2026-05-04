8570b4bd8e2e4780c9eadf45518aac2c34713d2e
asilva-ops <a.silvamuoz@gmail.com>
2026-05-04 12:53:53 -0400

    model created: estado mascota, usuario y tipo mascota

--- src/main/java/com/example/mascotav/model/TipoMascota.java
+++ src/main/java/com/example/mascotav/model/TipoMascota.java
@@ -1,5 +1,41 @@
 package com.example.mascotav.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tipo_mascota")
 public class TipoMascota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tipo_mascota")
+    private Integer id;
+
+    @NotBlank(message = "El nombre de la mascota es obligatorio")
+    @Size(min = 10, max = 15, message = "El nombre debe tener al menos 4 caracteres")
+    @Column(name = "nombreTipoMascota", length = 15, nullable = false)
+    private String nombreTipoMascota;
+
+    // Relaciones
 
 }
--- src/main/java/com/example/mascotav/model/Usuario.java
+++ src/main/java/com/example/mascotav/model/Usuario.java
@@ -1,5 +1,32 @@
 package com.example.mascotav.model;
 
+package com.example.mascotav.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    @Size(min = 5, max = 15, message = "El nombre debe tener entre 5 y 15 caracteres")
+    @Column(name = "nombre_user", length = 15, nullable = false)
+    private String nombreUsuario;
+
+    @Column(name = "fecha_creacion", updatable = false)
+    private LocalDateTime fechaCreacion;
 
 }
