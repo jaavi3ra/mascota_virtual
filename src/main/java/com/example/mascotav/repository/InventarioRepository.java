7bb51cf5ec362dce327b5a0c4cf300e20d8a9893
jaavi3ra <javi3ra20@gmail.com>
2026-05-06 16:07:00 -0400

    adds intefaces: Evo,Nivel,Tda_Item,user_tida_item

--- src/main/java/com/example/mascotav/repository/InventarioRepository.java
+++ src/main/java/com/example/mascotav/repository/InventarioRepository.java
@@ -6,14 +6,13 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.mascotav.model.Inventario;
-import com.example.mascotav.model.Tienda_item;
-import com.example.mascotav.model.Usuario;
+
 
 @Repository
 public interface InventarioRepository extends JpaRepository<Inventario, Integer>{
-    Optional<Inventario> findByUsuarioAndItem(Usuario usuario, Tienda_item item);  // Buscar un item de un usuario || Optional es contenedor que puede o no tener un valor
+    Optional<Inventario> findByUsuarioAndItem(Integer userId, Integer itemId);  // Buscar un item de un usuario || Optional es contenedor que puede o no tener un valor
 
-     List<Inventario> findByUsuario(Usuario usuario); // Obtener todo el inventario de un usuario
+     List<Inventario> findByUsuario(Integer userId); // Obtener todo el inventario de un usuario
 
-     void deleteByUsuarioAndItem(Usuario usuario, Tienda_item item); // elimnar un item de un usuario del inventario
+     void deleteByUsuarioAndItem(Integer userId, Integer itemId); // elimnar un item de un usuario del inventario
 }
