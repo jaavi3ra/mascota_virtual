ba91edce6589c4b58a9bda08801bdd4edfaeeda8
beenjajm01-lang <beenjajm01@gmail.com>
2026-05-05 22:11:18 -0400

    Modelos accion, historial de acciones, item y tienda terminados

--- src/main/java/com/example/mascotav/model/Tienda.java
+++ src/main/java/com/example/mascotav/model/Tienda.java
@@ -1,5 +1,38 @@
 package com.example.mascotav.model;
 
+import java.util.List;
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tienda")
 public class Tienda {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tienda")
+    private Integer idTienda;
+
+    @NotBlank(message = "El nombre de la tienda es obligatorio")
+    @Size(min = 3, max = 100, message = "El nombre de la tienda debe tener entre 3 y 100 caracteres")
+    @Column(name = "nom_tienda", length = 100, nullable = false)
+    private String nombreTienda;
+
+    // Relaciones
+    @OneToMany(mappedBy = "tienda")
+    private List<Tienda_item> tienda_items;
 }
