d0b0ac85f2869c397b93610903541ccbe11fe046
jaavi3ra <javi3ra20@gmail.com>
2026-05-10 17:25:24 -0400

    edit a /mvc: correcion de errores v1.2

--- src/main/java/com/example/mascotav/model/Mascota.java
+++ src/main/java/com/example/mascotav/model/Mascota.java
@@ -1,10 +1,6 @@
 package com.example.mascotav.model;
 
 import java.util.List;
-<<<<<<< HEAD
-
-=======
->>>>>>> feature/javi3ra
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
