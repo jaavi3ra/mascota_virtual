79e6c4d8a16b1e9991be9aae74dd2d3ffe11b3c1
jaavi3ra <javi3ra20@gmail.com>
2026-05-10 01:14:15 -0400

    errores corregidos

--- src/main/java/com/example/mascotav/model/Usuario_Tienda_Item.java
+++ src/main/java/com/example/mascotav/model/Usuario_Tienda_Item.java
@@ -1,7 +1,6 @@
 package com.example.mascotav.model;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
