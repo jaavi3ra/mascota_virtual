0a768a0f85eeb6bca955b762cb56362df367e5f5
beenjajm01-lang <beenjajm01@gmail.com>
2026-05-10 18:23:50 -0400

    guardando cambios de prueba antes de volver a mi rama

--- src/main/java/com/example/mascotav/DTO/UsuarioTiendaItemDTO.java
+++ src/main/java/com/example/mascotav/DTO/UsuarioTiendaItemDTO.java
@@ -1,23 +1,15 @@
 package com.example.mascotav.DTO;
 
-<<<<<<< HEAD
-import java.util.List;
-=======
 import java.time.LocalDateTime;
 import java.util.List;
 
 import com.example.mascotav.model.TiendaItem;
->>>>>>> feature/javi3ra
 import com.example.mascotav.model.Usuario;
 import lombok.Data;
 @Data
 public class UsuarioTiendaItemDTO {
     private Integer id;
     private Usuario usuario;
-<<<<<<< HEAD
-    private List<String> itemsdelatienda;
-=======
     private TiendaItem itemdelatienda;
     private LocalDateTime cooldown;
->>>>>>> feature/javi3ra
 }
