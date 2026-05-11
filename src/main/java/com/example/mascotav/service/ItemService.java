7b6abba1f8986c012c48a44f15809e508a387a99
beenjajm01-lang <beenjajm01@gmail.com>
2026-05-11 05:07:22 -0400

    feat: agregue metodo para testear y metodo de eliminar en Item service y controller

--- src/main/java/com/example/mascotav/service/ItemService.java
+++ src/main/java/com/example/mascotav/service/ItemService.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.mascotav.DTO.ItemDTO;
+import com.example.mascotav.model.Inventario;
 import com.example.mascotav.model.Item;
+import com.example.mascotav.model.Mascota;
+import com.example.mascotav.repository.InventarioRepository;
 import com.example.mascotav.repository.ItemRepository;
+import com.example.mascotav.repository.MascotaRepository;
+
 import jakarta.transaction.Transactional;
 
 @Service
@@ -14,6 +19,44 @@ import jakarta.transaction.Transactional;
 public class ItemService {
     @Autowired
     private ItemRepository itemRepository;
+
+    @Autowired
+    private InventarioRepository inventarioRepository;
+
+    @Autowired
+    private MascotaRepository mascotaRepository;
+
+    @Autowired
+    private AccionService accionService;
+
+    public String darItem(Integer userId, Integer  itemId, Integer idMascota) {
+        // Buscar el item en el inventario
+        Inventario inventario = inventarioRepository.findByUsuarioAndItem(userId, itemId)
+                    .orElseThrow(() -> new RuntimeException("No tienes este item en tu inventario"));
+        if(inventario.getCantidad() <= 0) {
+            throw new RuntimeException("No te quedan items");
+        }
+        // Buscar mascota
+        Mascota mascota = mascotaRepository.findById(idMascota)
+                .orElseThrow(() -> new RuntimeException("Mascota no encontrada"));
+
+        // Se ejecuta la accion
+        String resultadoAccion = accionService.ejecutarAccionDeItem(mascota, inventario.getItem());
+        
+        // Restar al inventario
+        inventario.setCantidad(inventario.getCantidad() - 1);
+        
+        // Se verifica si la cantidad es 0, si es asi lo borra
+        if(inventario.getCantidad() == 0) {
+            inventarioRepository.delete(inventario);
+        }else {
+            // Si es mayor a 0 lo guarda y se actualiza
+            inventarioRepository.save(inventario);
+        }
+
+        return resultadoAccion;
+
+    }
 
     public List<ItemDTO> obtenerTodos() {
         return itemRepository.findAll().stream()
@@ -23,6 +66,18 @@ public class ItemService {
 
     public Item guardar(Item item) {
         return itemRepository.save(item);
+    }
+
+    public void eliminar(Integer id) {
+        if(!itemRepository.existsById(id)) {
+            throw new RuntimeException("Error el item no existe");
+        }
+
+        try {
+            itemRepository.deleteById(id);
+        }catch (Exception e) {
+            throw new RuntimeException("No se puede eliminar el item");
+        }
     }
 
     private ItemDTO convertirADTO(Item item) {
