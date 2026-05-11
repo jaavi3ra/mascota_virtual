895cd921d74d6f0abc289e423c7e6820dbb94149
jaavi3ra <javi3ra20@gmail.com>
2026-05-11 04:05:55 -0400

    edit a /mvc: agregado item a tienda y item a inventario: falta tests

--- src/main/java/com/example/mascotav/controller/TiendaItemController.java
+++ src/main/java/com/example/mascotav/controller/TiendaItemController.java
@@ -3,14 +3,12 @@ package com.example.mascotav.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.mascotav.DTO.TiendaItemDTO;
 import com.example.mascotav.model.TiendaItem;
-import com.example.mascotav.repository.TiendaItemRepository;
 import com.example.mascotav.service.TiendaItemService;
 
 @RestController
@@ -21,7 +19,22 @@ public class TiendaItemController {
 
     @PostMapping("/agregarItemTienda")
     public ResponseEntity<?> agregarItemaTienda(@RequestBody TiendaItem tiendaItem){
+       try{
         tiendaItemService.agregarItemATienda(tiendaItem);
         return new ResponseEntity<>("Item agregado a la tienda!",HttpStatus.CREATED);
+       }catch(RuntimeException e){
+        return new ResponseEntity<>("No se pudo agregar :(", HttpStatus.BAD_REQUEST);
+       }
+    }
+
+    @PostMapping("/comprarItems/{isuser},{iditem}")
+    public ResponseEntity<?> comprarItem(@PathVariable Integer isuser, Integer iditem){
+       try {
+        tiendaItemService.comprarItem(isuser, iditem);
+        return new ResponseEntity<>("Item comprado y añadido al invetario!.",HttpStatus.CREATED);
+       } catch(RuntimeException e){
+        return new ResponseEntity<>("No se pudo comprar :(", HttpStatus.BAD_REQUEST);
+       }
+        
     }
 }
