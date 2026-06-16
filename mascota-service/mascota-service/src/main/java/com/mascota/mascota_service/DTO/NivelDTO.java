08103fc423f2e35493b9080df2cf6f30565a9268
jaavi3ra <javi3ra20@gmail.com>
2026-06-15 22:20:23 -0400

    dir no va en este bloque

--- mascota-service/mascota-service/src/main/java/com/mascota/mascota_service/DTO/NivelDTO.java
+++
@@ -1,9 +0,0 @@
-package com.mascota.mascota_service.DTO;
-
-import lombok.Data;
-
-@Data
-public class NivelDTO {
-    private Integer id_nivel;
-    private Integer exp_req;
-}
--- mascota-service/mascota-service/src/main/java/com/mascota/mascota_service/DTO/TipoMascotaDTO.java
+++
@@ -1,9 +0,0 @@
-package com.mascota.mascota_service.DTO;
-
-import lombok.Data;
-
-@Data
-public class TipoMascotaDTO {
-    private Integer idTipoMascota;
-    private String nomTipo;
-}
